9376a20f8030520c6fd63241f67692236cc2b6eb
Nika Oniani <nikaoniani2018@gmail.com>
2023-09-16 17:04:43 +0400

    added rotation feature

--- src/components/Earth/Earth.js
+++ src/components/Earth/Earth.js
@@ -1,6 +1,18 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 const Earth = () => {
+  const earthRef = useRef();
+
+  useFrame(() => {
+    if (earthRef.current) {
+      earthRef.current.rotation.y += 0.002;
+    }
+  });
+
   return (
-    <mesh>
+    <mesh ref={earthRef}>
+      <meshBasicMaterial wireframe={true} />
       <sphereGeometry />
     </mesh>
   );
