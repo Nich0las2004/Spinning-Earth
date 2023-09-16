648590c6a70830e4a726bc4a00248446ff688738
Nika Oniani <nikaoniani2018@gmail.com>
2023-09-16 16:57:01 +0400

    added a sphere

---
+++ src/components/Earth/Earth.js
@@ -0,0 +1,9 @@
+const Earth = () => {
+  return (
+    <mesh>
+      <sphereGeometry />
+    </mesh>
+  );
+};
+
+export default Earth;
--- src/components/Scene/Scene.js
+++ src/components/Scene/Scene.js
@@ -3,9 +3,12 @@ import classes from "./Scene.module.css";
 
 import { Stars } from "@react-three/drei";
 
+import Earth from "../Earth/Earth";
+
 const Scene = () => {
   return (
     <Canvas className={classes.canvas}>
+      <Earth />
       <Stars />
     </Canvas>
   );
