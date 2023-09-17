a9d49e922f47276abd539a9331847909f00f6639
Nika Oniani <nikaoniani2018@gmail.com>
2023-09-17 15:57:27 +0400

    added a basic loading message when the page loads

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
 import Scene from "./components/Scene/Scene";
+import Loading from "./components/Loading/Loading";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
+
   return (
     <div
       id="canvas-container"
@@ -9,7 +19,7 @@ const App = () => {
         height: "100vh",
       }}
     >
-      <Scene />
+      {isLoading ? <Loading /> : <Scene />}
     </div>
   );
 };
